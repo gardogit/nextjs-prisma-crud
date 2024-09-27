419376039d8876d5aa74b094ed153af0b2c372e1
gardogit <edgardo.irm@gmail.com>
2024-09-27 17:25:12 -0300

    show dashboard tasks

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,23 @@
-function HomePage() {
+async function loadTask() {
+  const res = await fetch("http://localhost:3000/api/tasks");
+  const data = await res.json();
+  return data;
+}
+async function HomePage() {
+  const tasks = await loadTask();
   return (
-    <div>Tareas</div>
-  )
+    <section className="container mx-auto">
+      <div className="grid grid-cols-3 gap-3 mt-10">
+        {tasks.map((task) => (
+          <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
+            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
+            <p>{task.description}</p>
+            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default HomePage
+export default HomePage;
