c4608de8ed9eb84f79363304fd43ba94cd49481d
gardogit <edgardo.irm@gmail.com>
2024-09-27 17:01:01 -0300

    Create task from new

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,9 +1,48 @@
-"use client"
+"use client";
+import { useRouter } from "next/navigation";
 
 function NewPage() {
+  const router = useRouter();
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const title = e.target.title.value;
+    const description = e.target.description.value;
+    const res = await fetch("/api/tasks", {
+      method: "POST",
+      body: JSON.stringify({ title, description }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const data = await res.json();
+    router.push("/");
+  };
   return (
-    <div>Formulario de tareas</div>
-  )
+    <div className="h-screen flex justify-center items-center">
+      <form className="bg-slate-800 p-10 w-1/4" onSubmit={onSubmit}>
+        <label htmlFor="title" className="font-bold text-sm">
+          Título de la tarea
+        </label>
+        <input
+          type="text"
+          id="title"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+        />
+        <label htmlFor="description" className="font-bold text-sm">
+          Descripción de la tarea
+        </label>
+        <textarea
+          id="description"
+          className="border border-gray-400 p-2 mb-4 w-full text-black"
+          rows="3"
+        ></textarea>
+
+        <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
+          Crear
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default NewPage
+export default NewPage;
