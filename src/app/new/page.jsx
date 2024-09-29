0c7ff3c8a3097a0dead28a0f8ae4183af911d081
gardogit <edgardo.irm@gmail.com>
2024-09-29 14:49:56 -0300

    Updating tasks

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,20 +1,46 @@
 "use client";
 import { useRouter } from "next/navigation";
+import { useState, useEffect } from "react";
 
-function NewPage() {
+function NewPage({ params }) {
   const router = useRouter();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+  useEffect(() => {
+    if (params.id) {
+      fetch(`/api/tasks/${params.id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setTitle(data.title);
+          setDescription(data.description);
+        });
+    }
+  }, [params.id]);
+
   const onSubmit = async (e) => {
     e.preventDefault();
-    const title = e.target.title.value;
-    const description = e.target.description.value;
-    const res = await fetch("/api/tasks", {
-      method: "POST",
-      body: JSON.stringify({ title, description }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-    const data = await res.json();
+
+    if (params.id) {
+      const res = await fetch(`/api/tasks/${params.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+    } else {
+      const res = await fetch("/api/tasks", {
+        method: "POST",
+        body: JSON.stringify({ title, description }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const data = await res.json();
+    }
+    router.refresh();
     router.push("/");
   };
   return (
@@ -27,6 +53,8 @@ function NewPage() {
           type="text"
           id="title"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
         <label htmlFor="description" className="font-bold text-sm">
           Descripción de la tarea
@@ -35,6 +63,8 @@ function NewPage() {
           id="description"
           className="border border-gray-400 p-2 mb-4 w-full text-black"
           rows="3"
+          onChange={(e) => setDescription(e.target.value)}
+          value={description}
         ></textarea>
 
         <button className="bg-blue-500 hover:bg-blue-700 text-white py-2 px-4 rounded">
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,5 @@
+import TaskCard from "../components/TaskCard";
+
 async function loadTask() {
   const res = await fetch("http://localhost:3000/api/tasks");
   const data = await res.json();
@@ -9,11 +11,7 @@ async function HomePage() {
     <section className="container mx-auto">
       <div className="grid grid-cols-3 gap-3 mt-10">
         {tasks.map((task) => (
-          <div key={task.id} className="bg-slate-900 p-3 hover:bg-slate-800 hover:cursor-pointer">
-            <h3 className="font-bold text-2xl mb-2">{task.title}</h3>
-            <p>{task.description}</p>
-            <p>{new Date(task.createdAt).toLocaleDateString()}</p>
-          </div>
+          <TaskCard key={task.id} task={task} />
         ))}
       </div>
     </section>
